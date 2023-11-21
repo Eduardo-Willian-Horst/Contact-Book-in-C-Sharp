78f58ac3500e2564ea97a17729a96c4ea2000c02
vitorzera <victorc.cole09@gmail.com>
2023-11-21 19:40:10 -0300

    Criacao de novos metodos

--- Domain/Empresarial.cs
+++ Domain/Empresarial.cs
@@ -6,9 +6,9 @@ class Empresarial : Contato
     private string endereco;
 
 
-    public Empresarial(int id, string nome, int phoneNumber,string email ,string endereco) 
+    public Empresarial(int id, string nome, int phoneNumber, string email, string endereco)
     : base(id, nome, phoneNumber)
-    {   
+    {
         Set_Email(email);
         Set_Endereco(endereco);
     }
@@ -23,7 +23,7 @@ class Empresarial : Contato
     {
         this.endereco = endereco;
     }
-    
+
     private string Get_Email()
     {
         return email;
@@ -32,5 +32,23 @@ class Empresarial : Contato
     public string Get_Endereco()
     {
         return endereco;
+    }
+
+    // Método de negócio: Verifica se o e-mail é válido
+    public bool IsEmailValido()
+    {
+        // Lógica para verificar a validade do e-mail
+        // Aqui, uma implementação simples que verifica se o e-mail contém "@" e "."
+        return Email.Contains("@") && Email.Contains(".");
+    }
+
+    // Método de negócio: Imprime informações detalhadas do contato empresarial
+    public void ImprimirInformacoesDetalhadas()
+    {
+        Console.WriteLine($"ID: {Id}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Telefone: {PhoneNumber}");
+        Console.WriteLine($"E-mail: {Email}");
+        Console.WriteLine($"Endereço: {Endereco}");
     }
 }
